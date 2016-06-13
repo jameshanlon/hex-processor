b42bbf2b56a1a763b54d7cde666b7c7c2c01e94f
James Hanlon <jameswhanlon@gmail.com>
2016-06-13 16:01:36 +0100

    Compiler work.

--- compiler.cpp
+++ compiler.cpp
@@ -1,37 +1,20 @@
-#include <iostream>
 #include <cassert>
 #include <cstdlib>
-
-class Lex;
-class Lex::Token;
-
-// Symbol table.
-class Table {
-  std::map<std::string, Lex::Token> table;
-public:
-  void insert(const std::string &name, const Lex::Token token) {
-    table.insert(std::make_pair(name, token));
-  }
-  Lex::Token lookup(const std::string &name) {
-    std::map<std::string, Lex::Token>::const_iterator it = table.find(name);
-    if(it != table.end())
-      return it->second;
-    table.insert(std::make_pair(name, Lex::Token::NAME));
-    return Lex::Token::NAME;
-  }
-};
+#include <map>
+#include <vector>
+#include <iostream>
 
 // The lexical analyser
 class Lex {
   const int BUFFER_SIZE = 64;
   Table table;
-  FILE* fp;
+  std::ifstream &file;
   int lineNum;
   int linePos;
   int value;
   std::string s;
   char ch;
-  int chCount;
+  int count;
   char buffer[BUFFER_SIZE];
   bool nlPending;
 
@@ -56,13 +39,13 @@ class Lex {
   }
 
   void readChar() {
-    ch = std::fgetc(fp);
-    chBuf[++chCount % BUFFER_SIZE] = ch;
+    ch = std::fgetc(file);
+    chBuf[++count % BUFFER_SIZE] = ch;
   }
 
   void printChBuf() {
     std::cout << "\n...";
-    for(int i = chCount + 1; i < chCount + BUFFER_SIZE; ++i) {
+    for(int i = count + 1; i < count + BUFFER_SIZE; ++i) {
       if (char c = chBuf[i % BUFFER_SIZE]) {
         std::cout << c;
       }
@@ -173,7 +156,7 @@ class Lex {
   }
 
 public:
-  enum class {
+  enum class Token {
     EOF=1,
     ERROR,
     // Literals
@@ -195,17 +178,17 @@ public:
     WHILE,   CASE,
   } token;
 
-  Lex(Table &table, FILE *fp) :
+  Lex(Table &table, std::ifstream &file) :
       table(table),
-      fp(fp),
+      file(file),
       lineNum(1),
-      chCount(0) {
+      count(0) {
     declareKeywords();
     readChar();
   }
 
   Token readToken() {
-    Lex::Token tok;
+    Lex::Token token;
     switch(ch) {
 
     // Newlines (skip)
@@ -222,7 +205,7 @@ public:
     // Comment: #.* (skip)
     case '#':
       do readChar(); while (ch != EOF && ch != '\n');
-      if(ch=='\n') {
+      if(ch == '\n') {
         lineNum++;
       }
       readChar();
@@ -239,12 +222,12 @@ public:
       readChar();
       if (ch == 'x') {
         readHexInt();
-        tok = Token::HEXINT;
+        token = Token::HEXINT;
         break;
       }
       if (ch == 'b') {
         readBinInt();
-        tok = Token::BININT;
+        token = Token::BININT;
         break;
       }
       error("expected 0x or 0b literal");
@@ -267,52 +250,52 @@ public:
       return (Lex::Token) table.lookup(s);
 
     // Symbols
-    case '{': tok = Token::LCURLY;  break;
-    case '}': tok = Token::RCURLY;  break;
-    case '[': tok = Token::LSQ;     break;
-    case ']': tok = Token::RSQ;     break;
-    case '(': tok = Token::LPAREN;  break;
-    case ')': tok = Token::RPAREN;  break;
-    case ',': tok = Token::COMMA;   break;
-    case '.': tok = Token::DOT;     break;
-    case ';': tok = Token::SEMI;    break;
-    case '=': tok = Token::EQ;      break;
-    case '+': tok = Token::ADD;     break;
-    case '-': tok = Token::SUB;     break;
-    case '*': tok = Token::MUL;     break;
-    case '/': tok = Token::DIV;     break;
-    case '^': tok = Token::XOR;     break;
-    case '%': tok = Token::REM;     break;
+    case '{': token = Token::LCURLY;  break;
+    case '}': token = Token::RCURLY;  break;
+    case '[': token = Token::LSQ;     break;
+    case ']': token = Token::RSQ;     break;
+    case '(': token = Token::LPAREN;  break;
+    case ')': token = Token::RPAREN;  break;
+    case ',': token = Token::COMMA;   break;
+    case '.': token = Token::DOT;     break;
+    case ';': token = Token::SEMI;    break;
+    case '=': token = Token::EQ;      break;
+    case '+': token = Token::ADD;     break;
+    case '-': token = Token::SUB;     break;
+    case '*': token = Token::MUL;     break;
+    case '/': token = Token::DIV;     break;
+    case '^': token = Token::XOR;     break;
+    case '%': token = Token::REM;     break;
 
     case '~':
       readChar();
-      if(ch == '=') { tok = Token::NEQ; break; }
+      if(ch == '=') { token = Token::NEQ; break; }
       return Token::NOT;
 
     case '<':
       readChar();
-      if (ch == '=') { tok = Token::LEQ; break; }
-      if (ch == '<') { tok = Token::LSH; break; }
+      if (ch == '=') { token = Token::LEQ; break; }
+      if (ch == '<') { token = Token::LSH; break; }
       return Token::LT;
 
     case '>':
       readChar();
-      if (ch == '=') { tok = Token::GEQ; break; }
-      if (ch == '>') { tok = Token::RSH; break; }
+      if (ch == '=') { token = Token::GEQ; break; }
+      if (ch == '>') { token = Token::RSH; break; }
       return Token::GT;
 
     case ':':
       readChar();
-      if (ch == '=') { tok = Token::ASS; break; }
+      if (ch == '=') { token = Token::ASS; break; }
       return Token::COLON;
 
     case '&':
       readChar();
-      if(ch == '&') { tok = Token::LAND; break; }
+      if(ch == '&') { token = Token::LAND; break; }
       return Token::AND;
 
     case '|':
-      if(ch == '|') { tok = Token::LOR; break; }
+      if(ch == '|') { token = Token::LOR; break; }
       return Token::OR;
 
     case '"':
@@ -320,14 +303,14 @@ public:
       while (ch != '"' && ch != EOF) {
         s += readStrCh();
       }
-      tok = Token::STR;
+      token = Token::STR;
       break;
 
     case '\'':
       readChar();
       value = static_cast<int>(ch);
       ch = readStrCh();
-      if(ch == '\'') { tok = Token::CHAR; break; }
+      if(ch == '\'') { token = Token::CHAR; break; }
       error("expected ''' after character constant");
       return Token::ERROR;
 
@@ -343,90 +326,116 @@ public:
     }
 
     readChar();
-    return tok;
+    return token;
   }
 
   void printToken(Token token) {
-    printf("token %3d %s ", (int) t, tokStr(token));
-    if(t == Lex::Token::NAME)   printf("%s", s.c_str());
-    if(t == Lex::Token::STR)    printf("%s", s.c_str());
-    if(t == Lex::Token::DECINT) printf("%d", value);
-    if(t == Lex::Token::HEXINT) printf("%x", value);
-    if(t == Lex::Token::BININT) {
-      int val = value;
-      char s[BUF_SIZE+1];
-      char *p = s + BUF_SIZE;
-      do { *--p = '0' + (val & 1); } while (val >>= 1);
-      printf("%s", s);
+    std::cout << "token " << t << " " << tokStr(token);
+    switch (t) {
+    case Lex::Token::NAME:
+      std::cout << s;
+      break;
+    case Lex::Token::STR:
+      std::cout << s;
+      break;
+    case Lex::Token::DECINT:
+      std::cout << value;
+      break;
+    case Lex::Token::HEXINT:
+      std::cout << std::hex << value << std::dec;
+      break;
+    case Lex::Token::BININT: {
+        int val = value;
+        char s[BUF_SIZE + 1];
+        char *p = s + BUF_SIZE;
+        do { *--p = '0' + (val & 1); } while (val >>= 1);
+        std::cout << s;
+        break;
+      }
     }
-    printf("\n");
+    std::cout << std::endl;
   }
 
   const char *Lex::tokStr(Lex::Token t) {
     switch(t) {
-      default:              return "unknown";
-      case Token::ERROR:    return "error";
-      case Token::EOF:      return "EOF";
+      default:            return "unknown";
+      case Token::ERROR:  return "error";
+      case Token::EOF:    return "EOF";
       // Literals
-      case Token::DECINT:   return "decimal";
-      case Token::HEXINT:   return "hexdecimal";
-      case Token::OCTINT:   return "octal";
-      case Token::BININT:   return "binary";
-      case Token::NAME:     return "name";
+      case Token::DECINT: return "decimal";
+      case Token::HEXINT: return "hexdecimal";
+      case Token::BININT: return "binary";
+      case Token::NAME:   return "name";
       // Symbols
-      case Token::LCURLY:   return "{";
-      case Token::RCURLY:   return "}";
-      case Token::LSQ:      return "[";
-      case Token::RSQ:      return "]";
-      case Token::LPAREN:   return "(";
-      case Token::RPAREN:   return ")";
-      case Token::COMMA:    return ",";
-      case Token::DOT:      return ".";
-      case Token::SEMI:     return ";";
-      case Token::EQ:       return "=";
-      case Token::ADD:      return "+";
-      case Token::SUB:      return "-";
-      case Token::MUL:      return "*";
-      case Token::DIV:      return "/";
-      case Token::XOR:      return "^";
-      case Token::REM:      return "%";
-      case Token::NEQ:      return "~=";
-      case Token::NOT:      return "~";
-      case Token::LEQ:      return "<=";
-      case Token::LSH:      return "<<";
-      case Token::LT:       return "<";
-      case Token::GEQ:      return ">=";
-      case Token::RSH:      return ">>";
-      case Token::GT:       return ">";
-      case Token::ASS:      return ":=";
-      case Token::COLON:    return ":";
-      case Token::LAND:     return "&&";
-      case Token::AND:      return "&";
-      case Token::LOR:      return "||";
-      case Token::OR:       return "|";
-      case Token::STR:      return "string";
-      case Token::CHAR:     return "char";
+      case Token::LCURLY: return "{";
+      case Token::RCURLY: return "}";
+      case Token::LSQ:    return "[";
+      case Token::RSQ:    return "]";
+      case Token::LPAREN: return "(";
+      case Token::RPAREN: return ")";
+      case Token::COMMA:  return ",";
+      case Token::DOT:    return ".";
+      case Token::SEMI:   return ";";
+      case Token::EQ:     return "=";
+      case Token::ADD:    return "+";
+      case Token::SUB:    return "-";
+      case Token::MUL:    return "*";
+      case Token::DIV:    return "/";
+      case Token::XOR:    return "^";
+      case Token::REM:    return "%";
+      case Token::NEQ:    return "~=";
+      case Token::NOT:    return "~";
+      case Token::LEQ:    return "<=";
+      case Token::LSH:    return "<<";
+      case Token::LT:     return "<";
+      case Token::GEQ:    return ">=";
+      case Token::RSH:    return ">>";
+      case Token::GT:     return ">";
+      case Token::ASS:    return ":=";
+      case Token::COLON:  return ":";
+      case Token::LAND:   return "&&";
+      case Token::AND:    return "&";
+      case Token::LOR:    return "||";
+      case Token::OR:     return "|";
+      case Token::STR:    return "string";
+      case Token::CHAR:   return "char";
       // Keywords
-      case Token::DO:       return "do";
-      case Token::ELSE:     return "else";
-      case Token::FALSE:    return "false";
-      case Token::FOR:      return "for";
-      case Token::FUNC:     return "function";
-      case Token::IF:       return "if";
-      case Token::IS:       return "is";
-      case Token::PROC:     return "process";
-      case Token::RESULT:   return "result";
-      case Token::SKIP:     return "skip";
-      case Token::STEP:     return "step";
-      case Token::STOP:     return "stop";
-      case Token::THEN:     return "then";
-      case Token::TO:       return "to";
-      case Token::TRUE:     return "true";
-      case Token::VAL:      return "val";
-      case Token::VALOF:    return "valof";
-      case Token::VAR:      return "var";
-      case Token::WHILE:    return "while";
+      case Token::DO:     return "do";
+      case Token::ELSE:   return "else";
+      case Token::FALSE:  return "false";
+      case Token::FOR:    return "for";
+      case Token::FUNC:   return "function";
+      case Token::IF:     return "if";
+      case Token::IS:     return "is";
+      case Token::PROC:   return "process";
+      case Token::RESULT: return "result";
+      case Token::SKIP:   return "skip";
+      case Token::STEP:   return "step";
+      case Token::STOP:   return "stop";
+      case Token::THEN:   return "then";
+      case Token::TO:     return "to";
+      case Token::TRUE:   return "true";
+      case Token::VAL:    return "val";
+      case Token::VALOF:  return "valof";
+      case Token::VAR:    return "var";
+      case Token::WHILE:  return "while";
     }
+  }
+};
+
+// Symbol table.
+class Table {
+  std::map<std::string, Lex::Token> table;
+public:
+  void insert(const std::string &name, const Lex::Token token) {
+    table.insert(std::make_pair(name, token));
+  }
+  Lex::Token lookup(const std::string &name) {
+    std::map<std::string, Lex::Token>::const_iterator it = table.find(name);
+    if(it != table.end())
+      return it->second;
+    table.insert(std::make_pair(name, Lex::Token::NAME));
+    return Lex::Token::NAME;
   }
 };
 
